d4121fbcfd78a0470f8ed9e2a87c35f44b2ea251
Yvettemukeshimana2 <yvettemukeshimana79@gmail.com>
2024-06-06 10:10:09 +0200

    gallery design and responsive

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+ import React, { useState } from "react";
+ import cut from "./images/cut.png";
+ import fish from "./images/fish.png";
+ import cow from "./images/cow.png";
+ import monkey from "./images/monkey.png";
+  import { FaArrowRight } from "react-icons/fa";
+
+ 
+
+ const Gallery = () => {
+   const [hoveredIndex, setHoveredIndex] = useState(null);
+
+   const handleMouseEnter = (index) => {
+     setHoveredIndex(index);
+   };
+
+   const handleMouseLeave = () => {
+     setHoveredIndex(null);
+   };
+
+   const images = [
+     { src: cut, title: "FENNEC", subtitle: "FOX", location: "India" },
+     { src: fish, title: "Fish", subtitle: "Ocean", location: "World" },
+     { src: monkey, title: "Monkey", subtitle: "Jungle", location: "Africa" },
+     { src: cow, title: "Cow", subtitle: "Farm", location: "Everywhere" },
+   ];
+
+   return (
+     <div className="p-10 bg-black inset-0">
+       <div className="p-10 inset-0 rounded-3xl bg-red-700">
+         <div className="relative w-full h-full overflow-y-hidden">
+           <div className="relative top-0 left-0 w-full h-full overflow-hidden">
+             <div className="relative w-full h-full">
+               <div className="relative top-0 left-0 w-full h-full overflow-y-auto">
+                 <div className="font-poppins relative text-[80px] font-light leading-[100px] text-white text-left">
+                   <p>The designs represent both desktop and mobile view.</p>
+                   <p className="mt-40">
+                     To view interactions, run the designs in prototype mode.
+                   </p>
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+       </div>
+       <div className="flex">
+         {images.map((image, index) => (
+           <div
+             key={index}
+             className="flex-1 rounded-lg bg-opacity-75 relative overflow-hidden group"
+             onMouseEnter={() => handleMouseEnter(index)}
+             onMouseLeave={handleMouseLeave}
+           >
+             <img
+               src={image.src}
+               alt={image.title}
+               className="w-full  h-full transition-all duration-300 ease-out transform scale-109 group-hover:scale-110 group-hover:opacity-70 group-hover:blur-sm group-hover:brightness-90 group-hover:saturate-0"
+             />
+             <div className="absolute inset-0 bg-black bg-opacity-0 transition-opacity duration-300 ease-out group-hover:bg-opacity-70"></div>
+             <div
+               className={`absolute bottom-20 left-5 mb-4 ml-4 text-white  
+                  "opacity-100" : "opacity-0"
+               } transition-opacity duration-300 ease-in-out`}
+               style={{
+                 fontFamily: "DIN Condensed",
+                 fontSize: "28px",
+                 fontWeight: "700",
+                 lineHeight: "38px",
+                 textAlign: "left",
+                 transition: "transform 0.5s ease-in-out",
+                 transform:
+                   hoveredIndex === index
+                     ? "translateY(-20px)"
+                     : "translateY(0)",
+               }}
+             >
+               <p className="hidden md:block">{image.title}</p>
+               <p className="hidden md:block">{image.subtitle}</p>
+               <div
+                 className={`absolute left-1 top-24 text-white transition-opacity duration-300 ease-in-out 
+                   "opacity-100" : "opacity-0"
+                 }`}
+                 style={{
+                   fontFamily: "Poppins",
+                   fontSize: "15px",
+                   fontWeight: "400",
+                   lineHeight: "16.02px",
+                   textAlign: "left",
+                   width: "213px",
+                   height: "16px",
+                   gap: "0px",
+                   opacity: "0.7",
+                 }}
+               >
+                 <p className="hidden md:block">{image.location}</p>
+                 <div className="flex flex-col">
+                   {hoveredIndex === index && (
+                     <div className="flex space-x-2 mt-2 left-10">
+                       <p className="text-md text-cyan-400 hidden md:block">know more</p>
+                       <FaArrowRight className="text-cyan-400 mt-0 h-4 hidden md:block" />
+                     </div>
+                   )}
+                 </div>
+               </div>
+             </div>
+           </div>
+         ))}
+       </div>
+     </div>
+   );
+ };
+
+ export default Gallery;
---
+++ tailwind.config.js
@@ -0,0 +1,76 @@
+ /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,html}", // Ensure to include paths to all your template files
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+       "border": "url('/images/border.jpg')",
+      },
+      width: {
+        '1140px': '1140px',
+      },
+      height: {
+        '700px': '700px',
+      },
+      gap: {
+        '0px': '0px',
+      },
+      opacity: {
+        '0': '0',
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        '80px': '80px',
+      },
+      lineHeight: {
+        '100px': '100px',
+      },
+  
+      colors: {
+        customGreen: {
+          50: "#f0fdf4",
+          100: "#dcfce7",
+          200: "#bbf7d0",
+          300: "#86efac",
+          400: "#4ade80",
+          500: "#22c55e",
+          600: "#16a34a",
+          700: "#15803d",
+          800: "#166534",
+          900: "#14532d",
+          950: "#042f2e",
+        },
+        customTeal: {
+          50: "#f0fdfa",
+          100: "#ccfbf1",
+          200: "#99f6e4",
+          300: "#5eead4",
+          400: "#2dd4bf",
+          500: "#14b8a6",
+          600: "#0d9488",
+          700: "#0f766e",
+          800: "#115e59",
+          900: "#134e4a",
+        },
+        customCyan: {
+          50: "#e3fafc",
+          100: "#c5f6fa",
+          200: "#99e9f2",
+          300: "#66d9e8",
+          400: "#3bc9db",
+          500: "#22a6b3",
+          600: "#0d8fa5",
+          700: "#0a6e8c",
+          800: "#0a4c6e",
+          900: "#083f5b",
+          950: "#083344",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
